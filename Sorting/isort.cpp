46ee380bd33b7a4b49a06f48648628f6e96fb119
Sachin <67999058+its-sachin@users.noreply.github.com>
2021-06-07 22:39:16 +0530

    isort done

---
+++ Sorting/isort.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+
+
+void isort(int array[], int left, int right) {
+
+    for (int i= left+1; i<=right; i++){
+        int key = array[i];
+        int j = i-1;
+        while(j>=0 && array[j] > array[i]) {
+            array[j+1] = array[j];
+            j--;
+        }
+        array[j+1] = key;
+    }
+}
+
+
+int main(int argc, char const *argv[])
+{
+    int size=10;
+    int array[size] = {9,3,8,-1,33,-43,9,11,-8,0};
+    std::cout<<"ACTUAL: ";
+    for(int i=0;i<size;i++){
+        std::cout<<array[i]<<" ";
+    }
+
+    isort(array,0,size-1);
+
+    std::cout<<"SORTED: ";
+    for(int i=0;i<size;i++){
+        std::cout<<array[i]<<" ";
+    }
+
+    std::cout<<""<<std::endl;
+    return 0;
+}
---
+++ Sorting/msort.cpp
@@ -0,0 +1,8 @@
+void msort(int array[], int n, int left)
+
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
