d998946e8fbaf2bf41a7d653298741d4da38f8df
Sachin <67999058+its-sachin@users.noreply.github.com>
2021-06-08 00:14:27 +0530

    msort added

--- Sorting/isort.cpp
+++ Sorting/isort.cpp
@@ -7,7 +7,7 @@ void isort(int array[], int left, int right) {
     for (int i= left+1; i<=right; i++){
         int key = array[i];
         int j = i-1;
-        while(j>=0 && array[j] > array[i]) {
+        while(j>=0 && array[j] > key) {
             array[j+1] = array[j];
             j--;
         }
@@ -20,10 +20,12 @@ int main(int argc, char const *argv[])
 {
     int size=10;
     int array[size] = {9,3,8,-1,33,-43,9,11,-8,0};
+
     std::cout<<"ACTUAL: ";
     for(int i=0;i<size;i++){
         std::cout<<array[i]<<" ";
     }
+    std::cout<<""<<std::endl;
 
     isort(array,0,size-1);
 
--- Sorting/msort.cpp
+++ Sorting/msort.cpp
@@ -1,8 +1,72 @@
-void msort(int array[], int n, int left)
+#include <iostream>
+#include <limits>
+
+void merge(int array[], int left, int mid, int right) {
+    int n1 = mid-left+1;
+    int n2 = right-mid;
+
+    int l[n1+1];
+    int r[n2+1];
+
+    for(int i=0;i<n1;i++){
+        l[i] = array[left+i];
+    }
+
+    for(int i=0;i<n2;i++){
+        r[i] = array[mid+i+1];
+    }
+
+    l[n1] = std::numeric_limits<int>::max();
+    r[n2] = std::numeric_limits<int>::max();
+
+    int i=0;
+    int j=0;
+
+    for(int k=left; k<=right; k++){
+
+        if (l[i] <= r[j]){
+            array[k] = l[i];
+            i+=1;
+
+        }
+
+        else{
+            array[k] = r[j];
+            j+=1;
+        }
+    }
+}
+
+void msort(int array[], int left, int right) {
+
+    if(left<right) {
+        int mid = (right+left)/2;
+        msort(array,left,mid);
+        msort(array,mid+1,right);
+        merge(array,left,mid,right);
+    }
+
+}
 
 
 int main(int argc, char const *argv[])
 {
-    /* code */
+    int size=10;
+    int array[size] = {9,3,8,-1,33,-43,9,11,-8,0};
+
+    std::cout<<"ACTUAL: ";
+    for(int i=0;i<size;i++){
+        std::cout<<array[i]<<" ";
+    }
+    std::cout<<""<<std::endl;
+
+    msort(array,0,size-1);
+
+    std::cout<<"SORTED: ";
+    for(int i=0;i<size;i++){
+        std::cout<<array[i]<<" ";
+    }
+
+    std::cout<<""<<std::endl;
     return 0;
 }
