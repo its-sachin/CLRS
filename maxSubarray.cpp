e2328ca7d774cb90bb4bee59fc32423fd562d6e1
Sachin <67999058+its-sachin@users.noreply.github.com>
2021-06-15 00:11:34 +0530

    max subarray linear and nlogn done

--- maxSubarray.cpp
+++ maxSubarray.cpp
@@ -3,7 +3,23 @@
 
 using namespace std;
 
-int bruteForce(int array[], int size) {
+int print(int array[], int bound[3]){
+
+    cout<<"Max sum: "<<bound[2]<<endl;
+    
+
+    for (int i = bound[0]; i<= bound[1]; i++) {
+
+        cout<<array[i]<<" ";
+    }
+    cout<<"\n";
+
+}
+
+int* bruteForce(int array[], int size) {
+
+    //O(n^2)
+
     int left = 0;
     int right = 0;
 
@@ -24,17 +40,116 @@ int bruteForce(int array[], int size) {
         
     }
 
-    cout<<"Max sum: "<<max<<endl;
+    int* ans = new int[3];
+    ans[0]=left;
+    ans[1]=right;
+    ans[2]=max;
+    return ans;
     
-
-    for (int i = left; i<= right; i++) {
-
-        cout<<array[i]<<" ";
-    }
-    cout<<"\n";
     
 }
 
+int* crossing(int array[], int left, int mid, int right){
+
+    int l,r;
+
+    int leftSum = std::numeric_limits<int>::min();
+    int sum=0;
+
+    for (int i=mid; i>=left; i--){
+        sum+=array[i];
+        if (sum>leftSum){
+            leftSum = sum;
+            l = i;
+        }
+    }
+    int rightSum = std::numeric_limits<int>::min();
+    sum = 0;
+
+    for (int i=mid+1; i<= right;i++){
+        sum+=array[i];
+        if (sum>rightSum){
+            rightSum = sum;
+            r = i;
+        }
+    }
+
+    int* ans = new int[3];
+    ans[0]=l;
+    ans[1]=r;
+    ans[2]=leftSum+rightSum;
+    return ans;
+
+}
+
+
+int* recusrive(int array[], int left, int right){
+
+    // O(nlogn)
+
+    if(left==right){
+        int* ans = new int[3];
+        ans[0]=left;
+        ans[1]=right;
+        ans[2]=array[left];
+        return ans;
+    }
+
+    else{
+        int mid = (left+right)/2;
+
+        int* l = recusrive(array,left,mid);
+        int* r = recusrive(array,mid+1,right);
+        int* m = crossing(array,left,mid,right);
+
+        if (l[2]>=r[2] && l[2]>=m[2]){
+            return l;
+        }
+        if (r[2]>=m[2]){
+            return r;
+        }
+        return m;
+
+    }
+
+
+}
+
+int* linear(int array[], int n) {
+
+    //O(n)
+
+    int max = std::numeric_limits<int>::min();
+    int sum = std::numeric_limits<int>::min();
+
+    int left=0;
+    int right=0;
+    int firstPos=0;
+
+    for (int i=0;i<n;i++){
+
+        sum += array[i];
+        
+
+        if (sum>max){
+            max = sum;
+            left = firstPos;
+            right = i;
+        }
+
+        if (sum<0){
+            sum=0;
+            firstPos = i+1;
+        }
+    }
+
+    int* ans = new int[3];
+    ans[0]=left;
+    ans[1]=right;
+    ans[2]=max;
+    return ans;
+
+}
 
 int main(int argc, char const *argv[])
 {
@@ -45,6 +160,14 @@ int main(int argc, char const *argv[])
 
     size = 4;
     int array2[size] = {1,-4,3,-4};
-    bruteForce(array2,size);
+
+    int* ans = bruteForce(array2,size);
+    int* ans1 = recusrive(array2,0,size-1);
+    int* ans2 = linear(array2,size);
+
+    print(array2,ans);
+    print(array2,ans1);
+    print(array2,ans2);
+
     return 0;
 }
